61d2a6c0403a3b2aefbf902938efbdca5d8eca9d
Yanis Montacer <yanis.montacer@edu.em-lyon.com>
2025-04-26 17:38:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
-function animateSlider(duration) {
-  console.log("Animation slider simulée :", duration);
-}
+
 
 (async function () {
   try {
